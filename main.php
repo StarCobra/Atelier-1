8a6482596b50ea1c71a638b1a38f0e93ed582a1b
StarCobra <damienpoirot@yahoo.com>
2022-11-09 10:05:38 +0100

    View files (empty) and main update

--- main.php
+++ main.php
@@ -1,12 +1,25 @@
 <?php
+
 require_once 'vendor/autoload.php';
 
 // Configure database connectivity
 $config = parse_ini_file("conf/config.ini");
 
 $db = new \Illuminate\Database\Capsule\Manager();
+
 $db->addConnection($config);
 $db->bootEloquent();
 $db->setAsGlobal();
 
 $router = new \iutnc\mf\router\Router();
+
+$router->addRoute('home',        'list_galeriePub',  '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('connecte',    'list_galeriePriv', '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('connexion',   'view_connexion',   '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('inscription', 'view_inscription', '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('user',        'view_userProfil',  '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('image',       'view_img',         '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+
+$router->setDefaultRoute('list_galeriePub');
+
+$router->run();
---
+++ src/classes/mediaphotoapp/auth/mediaphotoAuthentification.php
@@ -0,0 +1,9 @@
+<?php
+namespace iutnc\mediaphotoapp\auth;
+
+use iutnc\mf\auth\AbstractAuthentification;
+
+class mediaphotoAuthentification extends AbstractAuthentification
+{  
+
+}
