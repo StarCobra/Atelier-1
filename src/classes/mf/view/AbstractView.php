7980dc7395678ac14ffa3cd3109bac006e4d0e24
Vigelos0 <akizsmar@gmail.com>
2022-11-09 17:44:16 +0100

    feature: Update.

--- src/classes/mf/view/AbstractView.php
+++ src/classes/mf/view/AbstractView.php
@@ -167,6 +167,7 @@ abstract class AbstractView
             
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${title}</title>
 	    ${styles}
     </head>
