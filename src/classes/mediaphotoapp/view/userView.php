18191fd49e061c7563ee9b48ed9a2b4886494a45
StarCobra <damienpoirot@yahoo.com>
2022-11-09 15:24:04 +0100

    userView done

--- src/classes/mediaphotoapp/view/userView.php
+++ src/classes/mediaphotoapp/view/userView.php
@@ -10,6 +10,8 @@ class UserView extends MediaphotoView implements Renderer
     {
         $user = $this->data[0];
         $infoProfil = "<h3>Mon profil</h3>\n<p>" . $user->fullname . "</p>\n<p>" . $user->username . "</p>\n<h3>Mes galeries</h3>\n<button>Ajouter galerie</button>";
+    
+        $finalView = "";
 
         $galleries = $this->data[1];
         foreach ($galleries as $v) {
@@ -17,19 +19,17 @@ class UserView extends MediaphotoView implements Renderer
             $picture = $v->galleryWhereAddPicture()->get();
             $tags = "";
 
-            for ($i=0; $i < count($picture); $i++) { 
-                $chemin = "upload/";
-                $chemin .= $picture[0]->file;
-                
-                $image = "<div>\n<img src = ".$chemin.">";
-            }
+            $image = "<div>\n<img src = upload/".$picture[0]->file.">\n";
+
             $description = $v->name . "<br>" . $user->username . "<br>";
             for ($i = 0; $i < count($tag); $i++) {
                 $tags .= $tag[$i]->name." ";
             }
             $description .= $tags . "<br></div>";
+
+            $finalView .= $image . $description;
         }
 
-        return $infoProfil . $image . $description;
+        return $infoProfil . $finalView;
     }
 }
