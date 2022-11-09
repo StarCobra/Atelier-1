de576a022178a08ac3589209d4528c23dd394920
StarCobra <damienpoirot@yahoo.com>
2022-11-09 15:02:28 +0100

    upgrade userView

--- main.php
+++ main.php
@@ -2,6 +2,8 @@
 
 require_once 'vendor/autoload.php';
 
+use iutnc\mediaphotoapp\auth\mediaphotoAuthentification;
+
 // Configure database connectivity
 $config = parse_ini_file("conf/config.ini");
 
@@ -13,13 +15,13 @@ $db->setAsGlobal();
 
 $router = new \iutnc\mf\router\Router();
 
-$router->addRoute('home',        'list_galeriePub',  '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
-$router->addRoute('connecte',    'list_galeriePriv', '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
-$router->addRoute('connexion',   'view_connexion',   '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
-$router->addRoute('inscription', 'view_inscription', '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
-$router->addRoute('user',        'view_userProfil',  '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
-$router->addRoute('image',       'view_img',         '\iutnc\mediaphotoapp\control\#', iutnc\mediaphotoapp\auth\mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+//$router->addRoute('home',        'list_galeriePub',  '\iutnc\mediaphotoapp\control\#', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+//$router->addRoute('connecte',    'list_galeriePriv', '\iutnc\mediaphotoapp\control\#', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+//$router->addRoute('connexion',   'view_connexion',   '\iutnc\mediaphotoapp\control\#', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+//$router->addRoute('inscription', 'view_inscription', '\iutnc\mediaphotoapp\control\#', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+$router->addRoute('user',        'view_userProfil',  '\iutnc\mediaphotoapp\control\userController', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
+//$router->addRoute('image',       'view_img',         '\iutnc\mediaphotoapp\control\#', mediaphotoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->setDefaultRoute('list_galeriePub');
+$router->setDefaultRoute('view_userProfil');
 
 $router->run();
--- src/classes/mediaphotoapp/view/userView.php
+++ src/classes/mediaphotoapp/view/userView.php
@@ -7,7 +7,29 @@ use iutnc\mf\view\Renderer;
 class UserView extends MediaphotoView implements Renderer
 {
     public function render(): string
-    {   
-        return '';
+    {
+        $user = $this->data[0];
+        $infoProfil = "<h3>Mon profil</h3>\n<p>" . $user->fullname . "</p>\n<p>" . $user->username . "</p>\n<h3>Mes galeries</h3>\n<button>Ajouter galerie</button>";
+
+        $galleries = $this->data[1];
+        foreach ($galleries as $v) {
+            $tag = $v->galleryWhereTags()->get();
+            $picture = $v->galleryWhereAddPicture()->get();
+            $tags = "";
+
+            for ($i=0; $i < count($picture); $i++) { 
+                $chemin = "upload/";
+                $chemin .= $picture[0]->file;
+                
+                $image = "<div>\n<img src = ".$chemin.">";
+            }
+            $description = $v->name . "<br>" . $user->username . "<br>";
+            for ($i = 0; $i < count($tag); $i++) {
+                $tags .= $tag[$i]->name." ";
+            }
+            $description .= $tags . "<br></div>";
+        }
+
+        return $infoProfil . $image . $description;
     }
 }
