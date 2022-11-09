4c2f71419a53f930769b8e760b40edc9ce3eb1e5
akrem_jomaa <jomaakrem@gmail.com>
2022-11-09 12:29:45 +0100

    adding home Page

--- src/classes/mediaphotoapp/model/gallery.php
+++ src/classes/mediaphotoapp/model/gallery.php
@@ -1,8 +1,5 @@
 <?php
 namespace iutnc\mediaphotoapp\model;
-
-use iutnc\mediaphotoapp\model\user;
-
 class Gallery extends \Illuminate\Database\Eloquent\Model 
 {
     protected $table = 'gallery';  
@@ -10,16 +7,16 @@ class Gallery extends \Illuminate\Database\Eloquent\Model
     public $timestamps = true;      
 
     public function user(){
-        return $this->belongsTo('user','user_id');
+        return $this->belongsTo('iutnc\mediaphotoapp\model\user','user_id');
     }
     public function galleryToAccess(){
-        return $this->belongsToMany('user','access_gallery_user','gallery_id','user_id');
+        return $this->belongsToMany('iutnc\mediaphotoapp\model\user','access_gallery_user','gallery_id','user_id');
     }
-    public function galleryWhereAddPicture(){
-        return $this->belongsToMany('picture','picture_to_gallery','gallery_id','picture_id');
+    public function galleryPictures(){
+        return $this->belongsToMany('iutnc\mediaphotoapp\model\picture','picture_to_gallery','gallery_id','picture_id');
     }
     public function galleryWhereTags(){
-        return $this->belongsToMany('tag','gallery_to_tag','gallery_id','tag_id');
+        return $this->belongsToMany('iutnc\mediaphotoapp\model\tag','gallery_to_tag','gallery_id','tag_id');
 
     }
 
--- src/classes/mediaphotoapp/view/homeView.php
+++ src/classes/mediaphotoapp/view/homeView.php
@@ -8,6 +8,23 @@ class HomeView extends MediaphotoView implements Renderer
 {
     public function render(): string
     {   
-        return '';
+        $publicGalleries=$this->data;
+        $html = "";
+
+
+        foreach ($publicGalleries as $v) {
+            $galleryPictures = $v->galleryPictures()->get();
+            $creator = $v->user()->first();
+            $galleryLength = count($galleryPictures);
+            $randomNumber = rand(0,$galleryLength);
+            $randomNumber1 = $randomNumber - 1;
+            $randomPicture = $galleryPictures[$randomNumber1];
+           
+                $html .= "<div> <img src="."upload/".$randomPicture->file."><p>$v->name</p><p>$creator->fullname</p></div>";
+            
+           
+        }
+
+        return $html;
     }
 }
