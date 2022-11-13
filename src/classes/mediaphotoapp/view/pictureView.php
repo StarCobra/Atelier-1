6a1f1200ac99d721cdbbfc40aa37afcab5dfe129
leosix10 <leo.six1@etu.univ-lorraine.fr>
2022-11-13 15:36:26 +0100

    feat : adding CSS for pictureView

--- src/classes/mediaphotoapp/view/pictureView.php
+++ src/classes/mediaphotoapp/view/pictureView.php
@@ -14,7 +14,7 @@ class PictureView extends MediaphotoView implements Renderer
         $html = "";
 
         
-        $html .= "<div><img src = upload/".$picture->file."></div>";
+        $html .= "<figure><div><img src = upload/".$picture->file."><nav><a href=''><button>&#9664;</button></a><a href=''><button>&#9654;</button></a></nav></div><aside>#lesTags (à faire)</aside></figure>";
         
 
         return $html;
