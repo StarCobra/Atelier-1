99b550734f8c8e2c71f0ef7cfe967a41a049a438
leosix10 <leo.six1@etu.univ-lorraine.fr>
2022-11-13 11:43:34 +0100

    bug updated

--- conf/media_photo.sql
+++ conf/media_photo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 10 nov. 2022 à 13:32
--- Version du serveur : 8.0.27
--- Version de PHP : 8.1.0
+-- Généré le : dim. 13 nov. 2022 à 10:36
+-- Version du serveur : 5.7.36
+-- Version de PHP : 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `access_user_gallery`;
 CREATE TABLE IF NOT EXISTS `access_user_gallery` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `gallery_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `access_user_gallery_gallery_id` (`gallery_id`),
   KEY `access_user_gallery_user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,24 +45,25 @@ CREATE TABLE IF NOT EXISTS `access_user_gallery` (
 
 DROP TABLE IF EXISTS `gallery`;
 CREATE TABLE IF NOT EXISTS `gallery` (
-  `gallery_id` int NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `status` tinyint(1) NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`gallery_id`),
   KEY `gallery_gallery_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `gallery`
 --
 
 INSERT INTO `gallery` (`gallery_id`, `name`, `description`, `created_at`, `updated_at`, `status`, `user_id`) VALUES
-(1, 'test1', 'come on', '1970-01-01 00:00:00', '2022-11-10 09:18:01', 0, 1),
-(2, 'watch-me1', 'vous allez trouver les meilleurs photos d', '2022-11-09 15:39:12', '2022-11-10 11:13:25', 0, 1);
+(1, 'test', 'come on', '1970-01-01 00:00:00', '2022-11-11 13:12:44', 0, 1),
+(2, 'watch-me12', 'vous allez trouver les meilleurs photos d', '2022-11-09 15:39:12', '2022-11-12 13:39:03', 0, 1),
+(3, 'EmptyGallery', 'TEST', '2022-11-10 14:36:26', '2022-11-10 14:36:26', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -72,13 +73,13 @@ INSERT INTO `gallery` (`gallery_id`, `name`, `description`, `created_at`, `updat
 
 DROP TABLE IF EXISTS `gallery_to_tag`;
 CREATE TABLE IF NOT EXISTS `gallery_to_tag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tag_id` int NOT NULL,
-  `gallery_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL,
+  `gallery_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `gallery_to_tag_tag_id` (`tag_id`),
   KEY `gallery_to_tag_gallery_id` (`gallery_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `gallery_to_tag`
@@ -87,8 +88,8 @@ CREATE TABLE IF NOT EXISTS `gallery_to_tag` (
 INSERT INTO `gallery_to_tag` (`id`, `tag_id`, `gallery_id`) VALUES
 (1, 1, 1),
 (2, 2, 1),
-(3, 1, 2),
-(4, 2, 2);
+(8, 25, 2),
+(9, 26, 2);
 
 -- --------------------------------------------------------
 
@@ -98,14 +99,14 @@ INSERT INTO `gallery_to_tag` (`id`, `tag_id`, `gallery_id`) VALUES
 
 DROP TABLE IF EXISTS `picture`;
 CREATE TABLE IF NOT EXISTS `picture` (
-  `picture_id` int NOT NULL AUTO_INCREMENT,
+  `picture_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` text,
   `file` text,
   `added_date` datetime DEFAULT NULL,
-  `gallery_id` int NOT NULL,
+  `gallery_id` int(11) NOT NULL,
   PRIMARY KEY (`picture_id`),
   KEY `picture_gallery_id` (`gallery_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `picture`
@@ -119,7 +120,14 @@ INSERT INTO `picture` (`picture_id`, `type`, `file`, `added_date`, `gallery_id`)
 (13, 'jpg', 'siberian-husky.jpg', NULL, 1),
 (14, 'jpg', 'siberian-husky.jpg', NULL, 1),
 (15, 'jpg', 'siberian-husky.jpg', NULL, 1),
-(16, 'jpg', 'siberian-husky.jpg', NULL, 1);
+(16, 'jpg', 'siberian-husky.jpg', NULL, 1),
+(17, 'jpg', '1144794.jpg', NULL, 2),
+(18, 'jpg', '1144794.jpg', NULL, 2),
+(19, 'jpg', '1144794.jpg', NULL, 2),
+(20, 'jpg', 'Fatalis2.jpg', NULL, 2),
+(21, 'jpg', '1140729.jpg', NULL, 2),
+(22, 'jpg', '1144794.jpg', NULL, 1),
+(23, 'jpg', '1144794.jpg', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -129,13 +137,13 @@ INSERT INTO `picture` (`picture_id`, `type`, `file`, `added_date`, `gallery_id`)
 
 DROP TABLE IF EXISTS `picture_to_tag`;
 CREATE TABLE IF NOT EXISTS `picture_to_tag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tag_id` int NOT NULL,
-  `picture_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL,
+  `picture_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `picture_to_tag_tag_id` (`tag_id`),
   KEY `picture_to_tag_picture_id` (`picture_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `picture_to_tag`
@@ -158,10 +166,10 @@ INSERT INTO `picture_to_tag` (`id`, `tag_id`, `picture_id`) VALUES
 
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE IF NOT EXISTS `tag` (
-  `tag_id` int NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
   PRIMARY KEY (`tag_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `tag`
@@ -170,7 +178,10 @@ CREATE TABLE IF NOT EXISTS `tag` (
 INSERT INTO `tag` (`tag_id`, `name`) VALUES
 (1, '#anime'),
 (2, '#action'),
-(3, '#characters');
+(3, '#characters'),
+(11, '#moi'),
+(25, '#lp'),
+(26, '#chats');
 
 -- --------------------------------------------------------
 
@@ -180,14 +191,14 @@ INSERT INTO `tag` (`tag_id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text,
   `fullname` text,
   `mail_address` text,
   `password` text,
-  `level` int NOT NULL,
+  `level` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
