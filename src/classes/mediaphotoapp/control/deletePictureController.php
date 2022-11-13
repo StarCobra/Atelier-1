c06b2752a39745bd72b4a3bb75873ce8c5ce37e1
Vigelos0 <akizsmar@gmail.com>
2022-11-13 12:44:59 +0100

    feature: Update.

--- src/classes/mediaphotoapp/control/deletePictureController.php
+++ src/classes/mediaphotoapp/control/deletePictureController.php
@@ -5,14 +5,21 @@ namespace iutnc\mediaphotoapp\control;
 use iutnc\mf\router\Router;
 use iutnc\mediaphotoapp\model\Picture;
 use iutnc\mf\control\AbstractController;
+use iutnc\mediaphotoapp\model\User;
+use iutnc\mediaphotoapp\auth\mediaphotoAuthentification;
 
  class DeletePictureController extends AbstractController  
 {
    public function execute(): void
-   {
+   {    
     $pictureId = Picture::find($_GET['id']);
+
+    $id = mediaphotoAuthentification::connectedUser();
+    
     $picture = \iutnc\mediaphotoapp\model\Picture::where('picture_id','=',$pictureId->picture_id)->delete();
 
-    Router::executeRoute('list_galeriePub');
+    $_GET['id'] = $id;
+
+    Router::executeRoute('view_userProfil', ["user_id", $_GET['id']]);
    }
 }
