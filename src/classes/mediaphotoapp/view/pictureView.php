2d725d08d79e4126deccb75daaa3fd3fb097ff19
StarCobra <damienpoirot@yahoo.com>
2022-11-14 18:18:58 +0100

    TagPicture

--- src/classes/mediaphotoapp/view/pictureView.php
+++ src/classes/mediaphotoapp/view/pictureView.php
@@ -13,21 +13,21 @@ class PictureView extends MediaphotoView implements Renderer
         $prevPicture = $this->data[0];
         $picture = $this->data[1];
         $nextPicture = $this->data[2];
-        // echo "$prevPicture->picture_id<br>";
-        // echo "$picture->picture_id<br>";
-        // echo "$nextPicture->picture_id<br>";
 
         $url_prevPic = $this->router->urlFor('pictureDetails', [['id', $prevPicture->picture_id]]);
         $url_nextPic = $this->router->urlFor('pictureDetails', [['id', $nextPicture->picture_id]]);
 
-        // echo $url_nextPic;
-        // echo $url_prevPic;
         $html = "";
 
 
-        $html .= "<figure><div><img src = upload/" . $picture->file . "><nav><a href='$url_prevPic'><button>&#9664;</button></a><a href='$url_nextPic'><button>&#9654;</button></a></nav></div><aside>#lesTags (à faire)</aside></figure>";
+        $html .= "<figure><div><img src = upload/" . $picture->file . "><nav><a href='$url_prevPic'><button>&#9664;</button></a><a href='$url_nextPic'><button>&#9654;</button></a></nav></div><figcaption>";
 
-
+        $pictureTags = $picture->pictureTags()->get();
+        foreach ($pictureTags as $v) {
+            $html .= "<span>$v->name</span>";
+        }
+        $html .= "</aside></figure>";
+    
         return $html;
     }
 }
